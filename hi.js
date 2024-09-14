848f0d3c65270ed5403e635b29f86f8649d43d0d
cetmulearnrvc <rosha3varghese@gmail.com>
2024-09-14 13:50:03 +0530

    First Commit

---
+++ hi.js
@@ -0,0 +1,13 @@
+var http=require('http')
+var fs=require('fs')
+http.createServer(function (req,res)
+{
+    fs.readFile('hello.html', function(err,data)
+    {
+        res.writeHeader(200,{'Content-Type':'text/html'})
+        res.write(data)
+        res.end()
+    })
+    
+    
+}).listen(1000)
