bd30d3eb67ece8f283d2e7825b14ebddae94224c
cetmulearnrvc <166170097+cetmulearnrvc@users.noreply.github.com>
2024-09-15 22:05:58 +0530

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,8 +1,9 @@
 var http=require('http')
 var fs=require('fs')
+var path=require('path')
 http.createServer(function (req,res)
 {
-    fs.readFile('hello.html', function(err,data)
+    fs.readFile(path.join(__dirname,'hello.html'), function(err,data)
     {
         res.writeHeader(200,{'Content-Type':'text/html'})
         res.write(data)
